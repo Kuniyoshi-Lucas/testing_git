c7cb6511e303523253ee1b1160e3d5cd3d70cb04
Lucas Kuniyoshi <lucaskuniyoshi@Lucass-MacBook-Pro-3.local>
2021-07-24 08:55:58 -0300

    Changing the py file
    Inserting a Factorial function

--- hello_word.py
+++ hello_word.py
@@ -1,1 +1,10 @@
+num = int(input('Factorial number:'))
+test = 1
+def fact(num):
+    if num == 1 or num == 0:
+        return 1
+    else:
+        return num*(fact(num - 1)) 
+x = fact(num)
 print('hello world')
+print(f'Factorial: {x}')
